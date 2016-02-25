c0c3ae45ccaed9b19c7fd3d481f06f145f81c1b8
Brian Redmond <brian.redmond@live.com>
2016-02-25 15:17:33 -0500

    Simulator complete. Adding DocDB Code next

--- retriever/src/retriever.py
+++ retriever/src/retriever.py
@@ -1,3 +1,8 @@
+"""
+This container retrieves values from Azure Service Bus Queue and writes them to DocumentDB
+Runs continuously and does not stop
+"""
+
 import os
 import random
 import socket
@@ -9,30 +14,34 @@ from log import Log
 from azure.servicebus import ServiceBusService, Message, Queue
 
 def doLoop():
+    # Write to log file and update Slack
     hostname = socket.gethostname()
-    logMessage = hostname + ": Processing Status @ " + str(time.ctime())
-    notify.info(logMessage)
-    # Write to log file
+    logMessage = "RETRIEVER (" + hostname + "): Pulling queue messages and writing to DB starting @ " + str(time.ctime())
     log = Log()
     log.info(logMessage)
+    notify.info(logMessage)
+    
+    # Connect to Azure Service Bus
     sb_service = ServiceBusService(service_namespace='acslogging',shared_access_key_name='RootManageSharedAccessKey',shared_access_key_value='gnLZ2ixKkXng7rNvaCbgl9ucxsEKK7vuD5QkLl1iemM=')
         
     while True:
         sbqueue = sb_service.get_queue('statistics')
         queuelength = sbqueue.message_count
-        log.info(str(queuelength))
         if queuelength<1:
             notify.info("No messages in Service Bus Queue")
+            log.info("No messages in Service Bus Queue")
         else:
+            # Gather variables
             rtnmsg = sb_service.receive_queue_message('statistics', peek_lock=False, timeout=30)
             data_body = str(rtnmsg.body)
-            log.info("Mesage body: " + data_body)
             data_deviceid = str(rtnmsg.custom_properties['deviceid'])
             data_temp = str(rtnmsg.custom_properties['temp'])
             data_pressure = str(rtnmsg.custom_properties['pressure'])
             data_humidity = str(rtnmsg.custom_properties['humidity'])
             data_windspeed = str(rtnmsg.custom_properties['windspeed'])
-            notify.info("Temperature: " + data_temp + " >Message: " + data_body)          
+            notify.info("Temperature: " + data_temp + " >Message: " + data_body)
+            # Write to Azure DocumentDB
+                      
                                 
 if __name__ == "__main__":
     doLoop()
--- simulator/src/simulator.py
+++ simulator/src/simulator.py
@@ -1,10 +1,6 @@
 """
-Runs continuously while container is in use. Writes simulated metrics to Azure Queue 
-# Config (pull these values from Docker env variables later)
-AZURE_STORAGE_ACCOUNT_NAME='briardockerstorage'
-AZURE_STORAGE_ACCOUNT_KEY='y3KzKM9Dql9hTJXZPBkW+VqQX88OKjKQPREbABljGJepjDYifxn8cJG5SzKmoyVQ8QnTFRfR+a6rd7eJ3iwdCw=='
-AZURE_STORAGE_QUEUE_NAME='briaracsqueue'
-SIMULATION_DELAY=15
+Writes simulated metrics to Azure Queue
+Runs continuously and does not stop 
 """
 
 import os
@@ -18,22 +14,56 @@ from log import Log
 from azure.servicebus import ServiceBusService, Message, Queue
 
 def doSomething():
+    # Write to log file and update Slack
     hostname = socket.gethostname()
-    logMessage = hostname + ": Processing Status @ " + str(time.ctime())
-    notify.info(logMessage)
-    # Write to log file
+    logMessage = "SIMULATOR (" + hostname + "): Creating statistics starting @ " + str(time.ctime())
     log = Log()
     log.info(logMessage)
+    notify.info(logMessage)
+    
+    # Start loop and write random messages to Azure Service Bus Queue
     x=0
     
     while True:
         x=x+1
         time.sleep(5)
         # Write message to SB Queue
-        sb_service = ServiceBusService(service_namespace='acslogging',shared_access_key_name='RootManageSharedAccessKey',shared_access_key_value='gnLZ2ixKkXng7rNvaCbgl9ucxsEKK7vuD5QkLl1iemM=')    
-        msg = Message(logMessage.encode("utf-8"))
-        msg.custom_properties={'deviceid':str(x),'temp':'80.8','pressure':'68.23','humidity':'50','windspeed':'12.5'}
-        sb_service.send_queue_message('statistics', msg)
+        sb_service = ServiceBusService(service_namespace='acslogging',shared_access_key_name='RootManageSharedAccessKey',shared_access_key_value='gnLZ2ixKkXng7rNvaCbgl9ucxsEKK7vuD5QkLl1iemM=')
+        msg_text = "Update number: " + str(x)    
+        msg = Message(msg_text.encode("utf-8"))
+        # Randomly create data for SB Queue
+        r = random.randint(1, 10)
+        if r == 1:
+            msg.custom_properties={'deviceid':'mydevice' + str(r),'temp':'40.8','pressure':'61.93','humidity':'20','windspeed':str(r) + '.5'}
+            sb_service.send_queue_message('statistics', msg)
+        elif r == 2: 
+            msg.custom_properties={'deviceid':'mydevice' + str(r),'temp':'50.8','pressure':'62.83','humidity':'30','windspeed':str(r) + '.5'}
+            sb_service.send_queue_message('statistics', msg)
+        elif r == 3: 
+            msg.custom_properties={'deviceid':'mydevice' + str(r),'temp':'60.8','pressure':'63.73','humidity':'40','windspeed':str(r) + '.5'}
+            sb_service.send_queue_message('statistics', msg)
+        elif r == 4: 
+            msg.custom_properties={'deviceid':'mydevice' + str(r),'temp':'70.8','pressure':'64.63','humidity':'50','windspeed':str(r) + '.5'}
+            sb_service.send_queue_message('statistics', msg)
+        elif r == 5: 
+            msg.custom_properties={'deviceid':'mydevice' + str(r),'temp':'80.8','pressure':'51.93','humidity':'60','windspeed':str(r) + '.5'}
+            sb_service.send_queue_message('statistics', msg)
+        elif r == 6: 
+            msg.custom_properties={'deviceid':'mydevice' + str(r),'temp':'90.8','pressure':'55.93','humidity':'70','windspeed':str(r) + '.5'}
+            sb_service.send_queue_message('statistics', msg)
+        elif r == 7: 
+            msg.custom_properties={'deviceid':'mydevice' + str(r),'temp':'45.2','pressure':'34.99','humidity':'45','windspeed':str(r) + '.5'}
+            sb_service.send_queue_message('statistics', msg)
+        elif r == 8: 
+            msg.custom_properties={'deviceid':'mydevice' + str(r),'temp':'55.2','pressure':'38.99','humidity':'55','windspeed':str(r) + '.5'}
+            sb_service.send_queue_message('statistics', msg)
+        elif r == 9:
+            msg.custom_properties={'deviceid':'mydevice' + str(r),'temp':'65.2','pressure':'36.99','humidity':'65','windspeed':str(r) + '.5'}
+            sb_service.send_queue_message('statistics', msg)
+        else:
+            msg.custom_properties={'deviceid':'mydevice' + str(r),'temp':'75.2','pressure':'71.77','humidity':'38','windspeed':str(r) + '.5'}
+            sb_service.send_queue_message('statistics', msg)
+         
         log.info("Message #" + str(x) + " posted.")
                
 if __name__ == "__main__":
