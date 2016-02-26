9f8e5fd23e0d7f15d21a500afed7189ef2522397
Brian Redmond <brian.redmond@live.com>
2016-02-26 15:04:35 -0500

    Finalized V1 with docker-compose

--- setup.sh
+++ setup.sh
@@ -3,13 +3,15 @@ docker build -t chzbrgr71/acs-simulator simulator
 docker build -t chzbrgr71/acs-retriever retriever
 docker build -t chzbrgr71/acs-slacker slacker
 
-docker run -d --name sim1 chzbrgr71/acs-simulator
-docker run -d --name sim2 chzbrgr71/acs-simulator
-docker run -d --name sim3 chzbrgr71/acs-simulator
-docker run -d --name sim4 chzbrgr71/acs-simulator
-docker run -d --name ret1 chzbrgr71/acs-retriever
-docker run -d --name ret2 chzbrgr71/acs-retriever
-docker run -d --name slack chzbrgr71/acs-slacker
+#docker run -d --name sim1 chzbrgr71/acs-simulator
+#docker run -d --name sim2 chzbrgr71/acs-simulator
+#docker run -d --name sim3 chzbrgr71/acs-simulator
+#docker run -d --name sim4 chzbrgr71/acs-simulator
+#docker run -d --name ret1 chzbrgr71/acs-retriever
+#docker run -d --name ret2 chzbrgr71/acs-retriever
+#docker run -d --name slack chzbrgr71/acs-slacker
 
 # Compose
-# docker-compose up -docker
+docker-compose up -d
+docker-compose scale simulate=4
+docker-compose scale retrieve=1
