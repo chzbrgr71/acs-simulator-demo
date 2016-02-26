5c96bf0fc06aa4fc85c3c35a3aab741ed4370060
Brian Redmond <brian.redmond@live.com>
2016-02-26 07:46:21 -0500

    Added docdb container

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 docker build -t chzbrgr71/acs-simulator simulator
 docker build -t chzbrgr71/acs-retriever retriever
 
-docker run -d --name sim1 chzbrgr71/acs-simulator
+# docker run -d --name sim1 chzbrgr71/acs-simulator
 # docker run -d --name sim2 chzbrgr71/acs-simulator
-# docker run -d --name retriever chzbrgr71/acs-retriever
+docker run -d --name get1 chzbrgr71/acs-retriever
 
 # Compose
 # docker-compose up -docker
